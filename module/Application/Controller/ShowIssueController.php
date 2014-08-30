98542d5dbf08ce83ba8a35c5743fc50a3da3981a
matbogus <maci@Macur.local>
2014-08-30 18:12:37 +0200

    podglad taska

---
+++ module/Application/Controller/ShowIssueController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ShowIssueController extends AbstractActionController
+{
+    public function showIssueAction(){
+    	// zwroc tam cos
+        return;
+    }
+}
