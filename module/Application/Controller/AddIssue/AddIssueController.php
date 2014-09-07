394d813c8efa4bd30fe1509bd94da9138f55b2c5
Marcin Radlak <mar.radlak@gmail.com>
2014-09-07 17:04:12 +0200

    Implementacja podstawowej akcji addIssue w kontrolerze.

--- module/Application/Controller/AddIssue/AddIssueController.php
+++ module/Application/Controller/AddIssue/AddIssueController.php
@@ -3,11 +3,49 @@
 namespace Application\Controller\AddIssue;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Application\Model\Domain\Issue;
+use Application\Form\IssueForm;
 
-class AddIssueController extends AbstractActionController
-{
-    public function addIssueAction(){
-    	// lista zagadnień
-        return;
+class AddIssueController extends AbstractActionController {
+
+    protected $_objectManager;
+
+    public function addIssueAction() {
+        $form = new IssueForm();
+
+        if ($this->getRequest()->isPost()) {
+            $issue = new Issue();
+            $form->setInputFilter($issue->getInputFilter());
+            $form->setData($this->getRequest()->getPost());
+
+            if ($form->isValid()) {
+                $data     = $form->getData();
+                $project  = $this->getObjectManager()->find('\Application\Model\Domain\Project', $data['project']);
+                $priority = $this->getObjectManager()->find('\Application\Model\Domain\IssuePriority', $data['issuePriority']);
+
+                $issue->setProject($project);
+                $issue->setSubject($data['subject']);
+                $issue->setDescription($data['description']);
+                $issue->setIssuePriority($priority);
+                $issue->setCreationTime(new \DateTime());
+
+                $this->getObjectManager()->persist($issue);
+                $this->getObjectManager()->flush();
+                $newId = $issue->getId();
+
+                return $this->redirect()->toRoute('home');
+            }
+        }
+
+        return array('form' => $form);
+    }
+
+    protected function getObjectManager() {
+        if (!$this->_objectManager) {
+            $this->_objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        }
+
+        return $this->_objectManager;
     }
 }
