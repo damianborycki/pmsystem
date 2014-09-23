6022dadf49fb709b30870cfbf3f94e59a0f3d270
Michał Stachowicz <m.stachowicz@uj.edu.pl>
2014-09-23 19:52:30 +0200

    uzupelnianie pol na podstronie z projektem

--- module/Application/Controller/ProjectController.php
+++ module/Application/Controller/ProjectController.php
@@ -11,9 +11,11 @@ class ProjectController extends AbstractActionController {
     protected $_objectManager;
 
     public function showAction(){
-        $id = $this->getEvent()->getRouteMatch()->getParam('id');
-
-        $view   = new ViewModel();
+        $id = $this->getEvent()->getRouteMatch()->getParam('project');
+		$project = $this->getObjectManager()->getRepository('\Application\Model\Domain\Project')->find($id);
+		$issues = $this->getObjectManager()->getRepository('\Application\Model\Domain\Issue')->findBy(array('project' => $id));
+		
+        $view   = new ViewModel(array('project' => $project, 'issues' => $issues));
         $view->setTemplate('Project/Show');
 
         return $view;
