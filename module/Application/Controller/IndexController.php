e4235c162ff78adadafcd00ddccee8f584a50ef4
Michał Stachowicz <m.stachowicz@uj.edu.pl>
2014-09-23 18:17:54 +0200

    lista z projektami na stronie glownej

--- module/Application/Controller/IndexController.php
+++ module/Application/Controller/IndexController.php
@@ -3,10 +3,25 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Application\Model\Domain\Project;
+use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction(){
-        return;
+        $projects  = $this->getObjectManager()->getRepository('\Application\Model\Domain\Project')->findAll();
+        
+        $view   = new ViewModel(array('projects' => $projects));
+        $view->setTemplate('Index');
+
+        return $view;
+    }
+    
+    protected function getObjectManager() {
+        if (!$this->_objectManager) {
+            $this->_objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        }
+
+        return $this->_objectManager;
     }
 }
