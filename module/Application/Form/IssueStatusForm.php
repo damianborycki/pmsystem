9a92298e576dc5c08a60bb17dc57edf4886a8940
wdragula <weronika.dragula@gmail.com>
2014-09-24 15:10:03 +0200

    Update IssueStatusForm.php

--- module/Application/Form/IssueStatusForm.php
+++ module/Application/Form/IssueStatusForm.php
@@ -52,7 +52,34 @@ class IssueStatusForm extends Form {
             )
         ));
         
-
+        $this->add(array(
+            'type' => 'Zend\Form\Element\MultiCheckbox',
+            'name' => 'IsActive',
+            'options' => array(
+                'value_options' => array(
+                    '1'=>''
+                ),
+            ),
+            'attributes' => array(
+                'value' => '0', //set checked to '1'
+                'uncheckedValue' => '0'
+            )
+        ));
+        
+         $this->add(array(
+            'name'       => 'position',
+            'attributes' => array(
+                'type'  => 'hidden'
+            ),
+        ));
+        
+         $this->add(array(
+            'type' => 'Zend\Form\Element\Textarea',
+            'name'       => 'description',
+            'attributes' => array(               
+                'placeholder' => 'Opis statusu...'
+            )
+        ));
         
 
         $this->add(array(
