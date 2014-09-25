177c07f313a55d32185aba778f32d65ed668ab0b
mnoworyta <mnoworyta@users.noreply.github.com>
2014-09-25 02:06:25 +0200

    Update EnumerationController.php

--- module/Application/Controller/EnumerationController.php
+++ module/Application/Controller/EnumerationController.php
@@ -22,6 +22,63 @@ class EnumerationController extends AbstractActionController{
         return $this->_objectManager;
     }
     
+    public function rebuild_positions_category(){
+       $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager'); 
+        $rebuild_positions = $objectManager
+                        ->createQuery('SELECT u FROM Application\Model\Domain\IssueCategory u ORDER BY u.position ASC')
+                        ->getResult();
+     
+     $i=1;
+     foreach($rebuild_positions as $positions){
+
+        $id = $positions->getId();
+        $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueCategory u SET u.position = $i  WHERE u.id=$id")
+                            ->execute();    
+        
+             $i++;     
+        }
+   } 
+   
+    public function rebuild_positions_priority(){
+       $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager'); 
+        $rebuild_positions = $objectManager
+                        ->createQuery('SELECT u FROM Application\Model\Domain\IssuePriority u ORDER BY u.position ASC')
+                        ->getResult();
+     
+     $i=1;
+     foreach($rebuild_positions as $positions){
+
+        $id = $positions->getId();
+        $objectManager ->createQuery("UPDATE Application\Model\Domain\IssuePriority  u SET u.position = $i  WHERE u.id=$id")
+                            ->execute();    
+        
+             $i++;     
+        }
+   } 
+    
+    public function rebuild_positions_activity(){
+       $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager'); 
+        $rebuild_positions = $objectManager
+                        ->createQuery('SELECT u FROM Application\Model\Domain\IssueActivity u ORDER BY u.position ASC')
+                        ->getResult();
+     
+     $i=1;
+     foreach($rebuild_positions as $positions){
+
+        $id = $positions->getId();
+        $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueActivity u SET u.position = $i  WHERE u.id=$id")
+                            ->execute();    
+        
+             $i++;     
+        }
+   } 
+    
     public function indexAction() {
     $objectManager = $this
         ->getServiceLocator()
@@ -86,6 +143,9 @@ class EnumerationController extends AbstractActionController{
                      
      $view = new ViewModel(array('issuelist'=> $issuelist,'issuepriority'=>$issuepriority, 'issueactivity'=>$issueactivity ));
      $view->setTemplate('Enumeration/index');
+     $this->rebuild_positions_category();
+     $this->rebuild_positions_priority();
+     $this->rebuild_positions_activity();
      return $view;
 }   
  
@@ -111,6 +171,7 @@ class EnumerationController extends AbstractActionController{
                                
                 
                 $issue->setName($data['name']);
+                $issue->setDescription($data['description']);
                 if (isset($data['IsActive'])) 
                     { $issue->setIsActive($data['IsActive'][0]);                                        
                     }
@@ -129,8 +190,13 @@ class EnumerationController extends AbstractActionController{
                      $issue->setIsDefault($data['IsDefault'][0]);
                     }
                 
+                $query = $objectManager->createQuery('SELECT COUNT(u.id) FROM Application\Model\Domain\IssueStatus u');
+                $count = $query->getSingleScalarResult();   
+                $issue->setPosition($count+1);  
+                
                 $this->getObjectManager()->persist($issue);                             
-                $this->getObjectManager()->flush();         
+                $this->getObjectManager()->flush();   
+                $this->rebuild_positions_category();
                 return $this->redirect()->toRoute('IssueCategory');
             }
       }
@@ -186,6 +252,8 @@ class EnumerationController extends AbstractActionController{
                 print_r($data);
                 
                 $issue->setName($data['name']);
+                $issue->setDescription($data['description']);
+                $issue->setPosition($data['position']); 
                  if (isset($data['IsActive'])) 
                     { 
                         $issue->setIsActive($data['IsActive'][0]);
@@ -211,7 +279,7 @@ class EnumerationController extends AbstractActionController{
                 $issue->setId($id);
                 $this->getObjectManager()->merge($issue);                  
                 $this->getObjectManager()->flush();        
-                
+                $this->rebuild_positions_category();
                 return $this->redirect()->toRoute('IssueCategory');
             }
       }
@@ -244,6 +312,7 @@ class EnumerationController extends AbstractActionController{
                                
                 
                 $issue->setName($data['name']);
+                $issue->setDescription($data['description']);
                 if (isset($data['IsActive'])) 
                     { $issue->setIsActive($data['IsActive'][0]);                                        
                     }
@@ -263,7 +332,8 @@ class EnumerationController extends AbstractActionController{
                     }
                 
                 $this->getObjectManager()->persist($issue);                             
-                $this->getObjectManager()->flush();         
+                $this->getObjectManager()->flush();  
+                $this->rebuild_positions_priority();
                 return $this->redirect()->toRoute('IssueCategory');
             }
       }
@@ -342,9 +412,11 @@ class EnumerationController extends AbstractActionController{
                     
                     
                 $issue->setId($id);
+                $issue->setDescription($data['description']);
+                $issue->setPosition($data['position']);  
                 $this->getObjectManager()->merge($issue);                  
                 $this->getObjectManager()->flush();        
-                
+                $this->rebuild_positions_priority();
                 return $this->redirect()->toRoute('IssueCategory');
             }
       }
@@ -379,6 +451,7 @@ class EnumerationController extends AbstractActionController{
                                
                 
                 $issue->setName($data['name']);
+                $issue->setDescription($data['description']);
                 if (isset($data['IsActive'])) 
                     { $issue->setIsActive($data['IsActive'][0]);                                        
                     }
@@ -397,8 +470,13 @@ class EnumerationController extends AbstractActionController{
                      $issue->setIsDefault($data['IsDefault'][0]);
                     }
                 
+                 $query = $objectManager->createQuery('SELECT COUNT(u.id) FROM Application\Model\Domain\IssueStatus u');
+                $count = $query->getSingleScalarResult();   
+                    
+                $issue->setPosition($count+1);
                 $this->getObjectManager()->persist($issue);                             
-                $this->getObjectManager()->flush();         
+                $this->getObjectManager()->flush();   
+                $this->rebuild_positions_activity();
                 return $this->redirect()->toRoute('IssueCategory');
             }
       }
@@ -476,7 +554,8 @@ class EnumerationController extends AbstractActionController{
                     }
    
                     
-                    
+                $issue->setDescription($data['description']);
+                $issue->setPosition($data['position']);     
                 $issue->setId($id);
                 $this->getObjectManager()->merge($issue);                  
                 $this->getObjectManager()->flush();        
@@ -489,6 +568,134 @@ class EnumerationController extends AbstractActionController{
       $view->setTemplate('Enumeration/edit_activity');
       return $view;
     }
+    
+    //up, down
+    
+    public function downcategoryAction()  {
+     $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager');   
+    
+  $id = (int) $this->params('id', null);
+    if (null === $id) {
+      return $this->redirect()->toRoute('IssueCategory');
+    }
+
+    $issueposition = $objectManager->find('Application\Model\Domain\IssueCategory', $id);  
+        $getposition = $issueposition->getPosition();
+     
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueCategory u SET u.position = $getposition  WHERE u.position=$getposition+1")
+                            ->execute();              
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueCategory u SET u.position = $getposition+1  WHERE u.id=$id")
+                            ->execute();   
+     $this->rebuild_positions_category();
+     return $this->redirect()->toRoute('IssueCategory');
+   } 
+   
+    public function upcategoryAction()   {
+     $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager');   
+    
+    $id = (int) $this->params('id', null);
+    if (null === $id) {
+      return $this->redirect()->toRoute('IssueCategory');
+    }
+
+    $issueposition = $objectManager->find('Application\Model\Domain\IssueCategory', $id);  
+    $getposition = $issueposition->getPosition();
+     
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueCategory u SET u.position = $getposition  WHERE u.position=$getposition-1")
+                            ->execute();              
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueCategory u SET u.position = $getposition-1  WHERE u.id=$id")
+                            ->execute(); 
+     $this->rebuild_positions_category();
+     return $this->redirect()->toRoute('IssueCategory');
+   } 
+   
+    public function downpriorityAction()  {
+     $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager');   
+    
+  $id = (int) $this->params('id', null);
+    if (null === $id) {
+      return $this->redirect()->toRoute('IssueCategory');
+    }
+
+    $issueposition = $objectManager->find('Application\Model\Domain\IssuePriority', $id);  
+        $getposition = $issueposition->getPosition();
+     
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssuePriority u SET u.position = $getposition  WHERE u.position=$getposition+1")
+                            ->execute();              
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssuePriority u SET u.position = $getposition+1  WHERE u.id=$id")
+                            ->execute();   
+     $this->rebuild_positions_category();
+     return $this->redirect()->toRoute('IssuePriority');
+   } 
+   
+    public function uppriorityAction()   {
+     $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager');   
+    
+    $id = (int) $this->params('id', null);
+    if (null === $id) {
+      return $this->redirect()->toRoute('IssueCategory');
+    }
+
+    $issueposition = $objectManager->find('Application\Model\Domain\IssuePriority', $id);  
+    $getposition = $issueposition->getPosition();
+     
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssuePriority u SET u.position = $getposition  WHERE u.position=$getposition-1")
+                            ->execute();              
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssuePriority u SET u.position = $getposition-1  WHERE u.id=$id")
+                            ->execute(); 
+     $this->rebuild_positions_category();
+     return $this->redirect()->toRoute('IssueCategory');
+   } 
+   
+    public function downactivityAction()  {
+     $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager');   
+    
+  $id = (int) $this->params('id', null);
+    if (null === $id) {
+      return $this->redirect()->toRoute('IssueCategory');
+    }
+
+    $issueposition = $objectManager->find('Application\Model\Domain\IssueActivity', $id);  
+        $getposition = $issueposition->getPosition();
+     
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueActivity u SET u.position = $getposition  WHERE u.position=$getposition+1")
+                            ->execute();              
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueActivity u SET u.position = $getposition+1  WHERE u.id=$id")
+                            ->execute();   
+     $this->rebuild_positions_category();
+     return $this->redirect()->toRoute('IssuePriority');
+   } 
+   
+    public function upactivityAction()   {
+     $objectManager = $this
+        ->getServiceLocator()
+        ->get('Doctrine\ORM\EntityManager');   
+    
+    $id = (int) $this->params('id', null);
+    if (null === $id) {
+      return $this->redirect()->toRoute('IssueCategory');
+    }
+
+    $issueposition = $objectManager->find('Application\Model\Domain\IssueActivity', $id);  
+    $getposition = $issueposition->getPosition();
+     
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueActivity u SET u.position = $getposition  WHERE u.position=$getposition-1")
+                            ->execute();              
+     $objectManager ->createQuery("UPDATE Application\Model\Domain\IssueActivity u SET u.position = $getposition-1  WHERE u.id=$id")
+                            ->execute(); 
+     $this->rebuild_positions_category();
+     return $this->redirect()->toRoute('IssueCategory');
+   } 
     
     
   } 
